3b7d8c840628aa2fd4537fcb074295217934c1d7
David Wiseman <dwiseman308@gmail.com>
2025-10-04 19:17:02 -0600

    Added a referance to boatign

---
+++ -Projects/Jnk/code.js
@@ -0,0 +1,22 @@
+// esc-to-index.js
+// Type the tower of gable shorthand to find the second hidden page
+(function() {
+  let keyBuffer = "";
+  const targetSequence = "tog";
+
+  window.addEventListener('keydown', function(e) {
+    const key = e.key.toLowerCase();
+    keyBuffer += key;
+
+    if (keyBuffer.length > targetSequence.length) {
+      keyBuffer = keyBuffer.slice(-targetSequence.length);
+    }
+
+    if (keyBuffer === targetSequence) {
+      window.location.replace('./Jnk/teo.html');
+      keyBuffer = ""; // Clear the buffer after successful match
+    } else if (!targetSequence.startsWith(keyBuffer)) {
+      keyBuffer = ""; // Clear the buffer if the sequence is broken
+    }
+  }, {capture:true});
+})();
